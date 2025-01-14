022285abf4bf4470de589a972c2afedc5aeee805
lisandro bustos <bustoslj16@gmail.com>
2025-01-14 02:04:01 -0300

    v4: finalizo mi proyecto agregando cards en Home y Realtimeproducts implementando socket.io y un form para agregar productos.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import handlebars from 'express-handlebars'
-import { Server as HttpServer } from 'http'
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server as HttpServer } from 'http';
 import { Server as SocketIo } from 'socket.io';
 import managerProducts from './managers/managerProducts.js';
 
@@ -8,35 +8,64 @@ import managerProducts from './managers/managerProducts.js';
 const productsManager = new managerProducts();
 
 // Importación de Rutas
-import viewsRouter from './routes/view.routes.js'
+import viewsRouter from './routes/view.routes.js';
 import productsRoutes from './routes/api/products.routes.js';
 import cartRoutes from './routes/api/cart.routes.js';
 
 // Configuro Servidor con Socket.io
 const app = express();
+const PORT = 8080;
 const httpServer = new HttpServer(app);
-const io = new SocketIo(httpServer)
+const io = new SocketIo(httpServer);
 
 // Middlewares de express
-app.use(express.json())
+app.use(express.json());
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Configuración de Handlebars
-app.engine('hbs', handlebars.engine({extname: '.hbs'}));
-app.set('view engine', 'hbs')
-app.set('views', 'src/views')
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
 // Rutas de express
-app.use('/', viewsRouter)
-app.use('/api/products', productsRoutes)
-app.use('/api/cart', cartRoutes)
+app.use('/', viewsRouter);
+app.use('/api/products', productsRoutes);
+app.use('/api/cart', cartRoutes);
 
-// Configuración del servidor con Socket.Io
-io.on('connection', (socket) => {
+// Configuración de socket.io
+io.on('connection', async (socket) => {
     console.log('Nuevo cliente conectado');
+    
+    // Emitir Lista de Productos
+    const products = await productsManager.getProducts();
+    socket.emit('products', products);
+
+    // Escucho si se agrega un producto
+    socket.on('newProduct', async (product) => {
+
+        // Valido el status como booleano (Por defecto en True)
+        if (typeof product.status !== 'boolean') {
+            product.status = true;
+        }
+
+        // Guardo el producto
+        const products = await productsManager.getProducts();
+        products.push(product);
+        await productsManager.saveProducts(products);
+        io.emit('products', products); // Emito la lista actualizada
+    });
+
+    // Escucho la eliminacion de un producot
+    socket.on('deleteProduct', async (productId) => {
+        const products = await productsManager.getProducts();
+        const updatedProducts = products.filter(p => p.id !== productId);
+        await productsManager.saveProducts(updatedProducts);
+        io.emit('products', updatedProducts); // Emito lista nueva
+    });
 });
 
-httpServer.listen(8080, () => {
-    console.log('Escuchando en puerto 8080');
-})
+
+httpServer.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -4,30 +4,22 @@ import managerProducts from "../managers/managerProducts.js";
 const router = Router();
 const productsManager = new managerProducts();
 
-// router.get('/', async (req, res) => {
-//     res.render('home', {products: []})
-// })
-
 // Render de la Lista de Productos en Home
 router.get('/', async (req, res) => {
     try {
         const products = await productsManager.getProducts();
-        res.render('home', {products});
+        res.render('home', { title: 'Home', products });
     } catch (error) {
         console.error('Lo sentimos. Hubo un error al cargar los Productos.', error);
         res.status(500).send({status: 'error', message: 'Error al cargar los Productos.'})
     }
 })
 
-// router.get('/realtimeproducts', (req, res)=>{
-//     res.render('realtimeproducts')
-// })
-
 // Render de la lista de Productos en RealTimeProducts
 router.get('/realtimeproducts', async (req, res) => {
     try {
         const products = await productsManager.getProducts();
-        res.render('realtimeproducts', { products });
+        res.render('realtimeproducts', { title: 'Productos en Tiempo Real', products });
     } catch (error) {
         console.error('Lo sentimos. Hubo un error al cargar los Productos.', error);
         res.status(500).send({status: 'error', message: 'Error al cargar los Productos.'});
